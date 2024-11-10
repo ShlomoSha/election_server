002c410d6d6c92f912ead481ba5018edadf27136
shlomoShalem <s0552288304@gmail.com>
2024-11-10 12:10:46 +0200

    created contorollers for all and routes for user and added these in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express  from "express";
 import 'dotenv/config'
+import userController from './controllers/users'
+import adminController from './controllers/admin'
+import votesController from './controllers/votes'
+import candidatesController from './controllers/candidates'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+app.use(express.json())
+
+app.use("/api/users", userController)
+app.use("/api/admin", adminController)
+app.use("/api/votes", votesController)
+app.use("/api/candidates", candidatesController)
 
 app.listen(PORT, () => {
     console.log(`Server started, Visit "http://localhost:${PORT}"`)
---
+++ src/routes/users.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
